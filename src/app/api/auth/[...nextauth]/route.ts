8c4014be0869b5fbec7e5fbbb59a22dada4af0c7
Jovanka <dev.kniamaps@outlook.com>
2025-05-20 19:46:26 +1000

    reverting changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,3 @@
 import { handlers } from "i/server/auth";
-import { authConfig } from "i/server/auth/config";
-import NextAuth from "next-auth";
-
-const handler = NextAuth(authConfig);
 
 export const { GET, POST } = handlers;
